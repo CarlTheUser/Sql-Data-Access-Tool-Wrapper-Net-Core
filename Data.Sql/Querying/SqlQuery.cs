7f37775163996b14d4f4c5b53146cec2e0c45c16
carlandres18@gmail.com <carlandres18@gmail.com>
2022-01-12 19:05:53 +0800

    Async SqlQuery abstract class

--- Data.Sql/Querying/SqlQuery.cs
+++ Data.Sql/Querying/SqlQuery.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Data.Sql.Querying
 {
     public abstract class SqlQuery<T>
     {
-        protected QueryFilter _filter = null;
+        protected QueryFilter? _filter = null;
 
         public SqlQuery<T> Filter(QueryFilter filter)
         {
@@ -14,5 +16,19 @@ namespace Data.Sql.Querying
         }
 
         public abstract IEnumerable<T> Execute();
+    }
+
+    public abstract class AsyncSqlQuery<T>
+    {
+        protected QueryFilter? _filter = null;
+
+        public AsyncSqlQuery<T> Filter(QueryFilter filter)
+        {
+            _filter = filter;
+
+            return this;
+        }
+
+        public abstract Task<IEnumerable<T>> ExecuteAsync(CancellationToken token);
     }
 }
